7aab5b3fd02fd02c4b83c0fea962854b9df911ed
unknown989 <55900066+unknown989@users.noreply.github.com>
2021-02-03 17:11:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from youtubesearchpython import ChannelsSearch
 import argparse
 import json
 
+# Algorithm to Transform a string to int (10K to 10000)
 def csn(x):
 	nmap = {"K":1000,"M":1000000}
 	x = str(x)
@@ -11,30 +12,45 @@ def csn(x):
 	else:
 		return int(x)
 
+# Parsing Arguments
 ap = argparse.ArgumentParser()
+# Adding arguments to the Parser
 ap.add_argument("-q","--query",required=True,help="Query to search for a channel")
 ap.add_argument("-l","--limit",required=True,help="Limit The search results")
 ap.add_argument("-s","--subscribers",required=False,help="(Optional) Subscribers count filter by ,default 500K")
+# Initialize Arguments tot args
 args = vars(ap.parse_args())
+# Load Subscribers Filter to 500K if the user didn't provide it ,if he/she did it will transform it to an integer
 subf = (500000 if not args["subscribers"] else int(args["subscribers"]))
 
+# Search Channels using the Query and The Limit arguments
 allSearch = ChannelsSearch(str(args["query"]), limit = int(args["limit"]))
 
-with open("test.json","w") as f:
-	f.write(json.dumps(allSearch.result()))
-
+# Main Loop
 for i in allSearch.result()["result"]:
+	# Getting the subscribers count to filter with
 	subs = csn(i["subscribers"].replace("subscribers","").replace(" ",""))
 	try:
+		# Checking if subscribers is greater than subscribers count 
 		if subs >= subf:
-			print(i["title"])
+			desc = list()
+			for o in i["descriptionSnippet"]:
+				desc.append(o["text"])
+			print(desc)
+			print("="*50)
+			print("Channel ID : "+i["id"])
+			print("Channel Name : "+i["title"])
+			print("Channel Subscribers : "+i["subscribers"])
+			print("Channel Videos Count : "+i["videoCount"])
+			print("Channel Description : "+" ".join(desc))
+			print("Channel Link : "+i["link"])
 		else:
 			pass
 	except Exception as e:
+		# Exception Handling , prints id ,title,type,subs
 		print("|>"+str(e))
 		print(i["id"])
 		print(i["title"])
 		print(i["type"])
 		print(i["subscribers"])
 		print("<|")
-
